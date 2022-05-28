7c86c6880e109495b4cca3ddd2dc058151876f77
Beksultan <bekafaceit1368@gmail.com>
2022-05-28 19:06:19 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+'use strict';
+const storeName = 'Adidas';
+const storeDescription = {
+    budget: 10000,
+    employess: [
+        'Beka',
+        'Anna',
+        'John',
+    ],
+    products: {
+        sneakers: 100,
+        shooz: 50,
+    },
+    open: true,
+};
